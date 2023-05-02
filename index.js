9ac2bf23bc3c99cbf485b2635f0bd2947a2080ad
shafaet <lbshaft01@gmail.com>
2023-05-02 15:49:39 +0600

    chefData api created

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefDatas = require("./data/chefDatas.json");
 
 app.get("/", (req, res) => {
-  res.send('restro is running');
+  res.send(chefDatas);
 });
-
 
 app.listen(port, () => {
   console.log(`Restro is running on port ${port}`);
