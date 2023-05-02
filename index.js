a98d5f0b613ecaa21b46f15d6b896481f1fc4d90
shafaet <lbshaft01@gmail.com>
2023-05-02 16:19:45 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefDatas = require("./data/chefDatas.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(chefDatas);
